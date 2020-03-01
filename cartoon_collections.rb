11f3e2ca242b2f1241ffb93b6884844de865bbe5
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:25:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   n=false
-  array.map{|word| n=true if word
+  array.map{|word| n=true if stringword
 end
 
 def find_the_cheese# code an argument here
