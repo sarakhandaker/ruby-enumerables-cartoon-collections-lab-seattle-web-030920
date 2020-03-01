abeaa9047ca47a099d4ba3a0ff77dfd88a24d4b2
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:13:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  array.each_with_index{ | item|
+  array.each_with_index{ | item, index|
     puts "#{index} #{item}"
   }
 end
