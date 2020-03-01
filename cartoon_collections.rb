c3a06ba5abcad54447ac3165bacdab14e1dc3d5b
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:31:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,12 @@ def long_planeteer_calls(array)# code an argument here
   n
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  n=nil
+  array.include{|word|
+  n=word if word=="cheddar"
+  }
+  n
 end
