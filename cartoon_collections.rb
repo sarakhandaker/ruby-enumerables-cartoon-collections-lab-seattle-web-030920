69410bf60cb01ba08be059cbc3063b0d98df30a9
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:57:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,7 @@ def find_the_cheese(array)# code an argument here
  x[0]=array.index("gouda")
  x[1]=array.index( "camembert")
  x[2]=array.index("cheddar")
-x.map{|item| item=4 if item==nil}
+x.map!{|item| item=4 if item==nil}
  n=x.min
 
    array[n] 
