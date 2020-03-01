b480cebc5a43153dc1c8c672733ce4c6d2c98a3b
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:12:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  each_with_index(array)
+  each_with_index{array
+  }
 end
 
 def summon_captain_planet# code an argument here
