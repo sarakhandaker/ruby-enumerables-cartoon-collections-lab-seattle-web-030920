d4c9f257c045cf4cc954d5d76b05781fe57b575b
Sara Khandaker <kttsara@hotmail.com>
2020-03-01 02:33:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  n=nil
-  array.index
-  n
+
+ n= array.index("cheddar" ||"gouda"||"camembert")
+  array[n]
 end
